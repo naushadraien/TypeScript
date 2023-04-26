a3ced5155e985eb446e020a0d902eac53c3a15e5
naushadraien <naushadraien5@gmail.com>
2023-04-26 22:20:14 +0545

    type array in typescript

---
+++ src/arrays.ts
@@ -0,0 +1,3 @@
+let numbers: number[] = [1, 2, 3, 4, '5']; //here 5 is not a number it is a string so it is showing the red line below the number 5
+
+numbers.forEach(n=> n.) // this will show the options in type script when we type . after anything
