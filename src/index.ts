f2402a3e89f186c67c33b2cfe8130c1b63ce4e09
naushadraien <naushadraien5@gmail.com>
2023-04-26 22:10:15 +0545

    type any in typescript

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ let sales: number = 123_456_789;
 let code: string = 'Typescript';
 let bool= false; // if we hover on the bool variable it will show the variable type that was defined previously
 let learn;
+
+function render(document : any){ //we can define the type of the arguments in the function like this
+    console.log(document);
+}
