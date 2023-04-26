0d449e96d4f0fb76686c0c39322136add4475bbd
naushadraien <naushadraien5@gmail.com>
2023-04-26 22:27:49 +0545

    tuples type in typescript

---
+++ src/tuples.ts
@@ -0,0 +1,3 @@
+//tuples type in typescript
+//tuples type in typescript is used when we have only two value one of number or other types and other one is string or othe types 
+let user: [number, string] = [1, 'hello'] //this is a tuple type
