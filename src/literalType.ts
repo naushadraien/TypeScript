e3606fad753c502421445ee2a67c70af0f4d1622
naushadraien <naushadraien5@gmail.com>
2023-04-28 11:12:57 +0545

    literal type in typescript

---
+++ src/literalType.ts
@@ -0,0 +1,7 @@
+//Literal means exact value or secific value
+
+type Qunatity = 50 | 100; //this is union type and this type defined is custom type and this is called literal type which has exact value or specific value
+
+let quantity: Qunatity = 50; //here we can only assign 50 or 100 to quantity variable 
+
+type Metric = '50' | '100'; //this is union type and this type defined is custom type
